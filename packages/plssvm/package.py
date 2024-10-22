2c4d05f8e468926965a8692f4ae7d6a0e9b40168
Alexander Van Craen <Alexander.Van-Craen@ipvs.uni-stuttgart.de>
2024-10-22 16:01:15 +0200

    add missing :

--- packages/plssvm/package.py
+++ packages/plssvm/package.py
@@ -514,7 +514,7 @@ class Plssvm(CMakePackage,CudaPackage,  ):
         if "+cuda" in self.spec:
             args += [self.define("PLSSVM_CUDA_TARGET_PLATFORMS", ["nvidia:sm_" + ",sm_".join(self.spec.variants["cuda_arch"].value)])]
         if "+hip" in self.spec:
-            if self.spec.variants["cuda_arch"].value != "none"
+            if self.spec.variants["cuda_arch"].value != "none":
                 args += [self.define("PLSSVM_HIP_TARGET_PLATFORMS", ["nvidia:sm_" + ",sm_".join(self.spec.variants["cuda_arch"].value)])]
             if  self.spec.variants["amdgpu_target"].value != "none":
                 args += [self.define("PLSSVM_HIP_TARGET_PLATFORMS", ["amd:" + ",".join(self.spec.variants["amdgpu_target"].value)])]
